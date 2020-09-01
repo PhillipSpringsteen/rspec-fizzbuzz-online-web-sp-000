efe334e558294352862ee44adec9f9d64712bed5
Oscar Phillip Springsteen <pspringsteen160@gmail.com>
2020-09-01 19:27:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
-    return "Fizz" # Go fizz
+    "Fizz" # Go fizz
   end
   if int % 5 == 0 # if the number int is divisible by 5
     return "Buzz" # Go Buzz
   end
+  return x
 end
